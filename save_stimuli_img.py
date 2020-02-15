16e4cb03f3ec8fe8351d14719f0e074a145c1cd4
ariaaay <ariayuanwang@gmail.com>
2020-02-15 18:45:17 -0500

    bug fix

--- save_stimuli_img.py
+++ save_stimuli_img.py
@@ -8,7 +8,7 @@ outpath = "/lab_data/tarrlab/common/datasets/NSD_images"
 stim = pd.read_pickle("/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl")
 name = stim['cocoId']
 
-imgs = h5py.File("/lab_data/tarrlab/common/datasets/NSD/nsddata_stimuli/stimuli/nsd/nsd_stimuli.hdf5")['imgBrick']
+imgs = h5py.File("/lab_data/tarrlab/common/datasets/NSD/nsddata_stimuli/stimuli/nsd/nsd_stimuli.hdf5", "r")['imgBrick']
 
 for i in tqdm(range(imgs.shape[0])):
     im = Image.fromarray(imgs[i, :, :, :])
