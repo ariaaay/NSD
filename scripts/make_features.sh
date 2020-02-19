134b151f3db5c672d6d46a9ff595e24f36d7bde1
ariaaay <ariayuanwang@gmail.com>
2020-02-19 00:35:13 -0500

    bug fix

--- scripts/make_features.sh
+++ scripts/make_features.sh
@@ -1,25 +1,24 @@
-TASKS="
-# autoencoder \
-# denoise \
-# segment25d \
-# segment2d \
-# curvature \
-# class_1000 \
-# class_places \
-# edge2d \
-# edge3d \
-# keypoint2d \
-# keypoint3d \
-# reshade \
-# rgb2depth \
-# rgb2mist \
-# rgb2sfnorm \
-# colorization \
-# room_layout \
-# segmentsemantic \
-# vanishing_point \
-# jigsaw \
-# inpainting_whole"
+TASKS="autoencoder \
+ denoise \
+ segment25d \
+ segment2d \
+ curvature \
+ class_1000 \
+ class_places \
+ edge2d \
+ edge3d \
+ keypoint2d \
+ keypoint3d \
+ reshade \
+ rgb2depth \
+ rgb2mist \
+ rgb2sfnorm \
+ colorization \
+ room_layout \
+ segmentsemantic \
+ vanishing_point \
+ jigsaw \
+ inpainting_whole"
 
 for task in $TASKS; do
  sbatch ~/NSD/scripts/generate_taskonomy_features.sh $task
