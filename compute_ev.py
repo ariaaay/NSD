01dc86b1ba0b14966db56642318d590159ef0219
ariaaay <ariayuanwang@gmail.com>
2020-02-27 13:11:41 -0500

    add plotting and voxelvise ev

--- compute_ev.py
+++ compute_ev.py
@@ -1,11 +1,13 @@
 import argparse
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 from util.util import ev
 
 # extract each subject's index for 1000 images
-def extract_subject_trials_index_common(subj):
+def extract_subject_trials_index_common(stim, subj):
     index = list()
     for i in range(3):
         col = 'subject%01d_rep%01d' % (subj, i)
@@ -15,17 +17,17 @@ def extract_subject_trials_index_common(subj):
     return index
 
 
-def extract_common_data_into_matrix(subj, roi_only=False):
-    if roi_only:
-        roi = "_roi_only"
-    else:
-        roi = ""
-    l = extract_subject_trials_index_common(subj)
+def compute_ev(stim, subj, roi=""):
+    l = extract_subject_trials_index_common(stim, subj)
     data = np.load("output/cortical_voxel_across_sessions_subj%02d%s.npy" % (subj, roi))
-    repeat = np.hstack((data[np.array(l[0])], data[np.array(l[1])], data[np.array(l[2])]))
-    print(repeat.shape)
-    assert repeat.shape == (1000,3)
-    return repeat
+    ev_list = []
+    for v in tqdm(range(data.shape[1])):
+        repeat = np.hstack((data[np.array(l[0]),v], data[np.array(l[1]),v], data[np.array(l[2]),v]))
+        assert repeat.shape == (1000,3)
+        ev_list.append(ev(repeat))
+
+    return ev_list
+
 
 
 if __name__ == "__main__":
@@ -34,8 +36,14 @@ if __name__ == "__main__":
     parser.add_argument("--subj", type=int)
 
     args = parser.parse_args()
+    if args.roi_only:
+        roi = "_roi_only"
+    else:
+        roi = ""
+
     stim = pd.read_pickle("/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl")
 
-    repeat_matrix = extract_common_data_into_matrix(args.subj, roi_only=args.roi_only)
-    out = ev(repeat_matrix)
-    print(out)
+    all_evs = compute_ev(stim, args.subj, roi)
+    np.save("output/evs_subj%02d%s.npy" % (args.subj, roi))
+    plt.hist(all_evs)
+    plt.savefig("figures/evs_subj%02d%s.npy")
