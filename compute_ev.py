dac3276f7d17666645e3b6c10fa99cee7b6d094c
ariaaay <ariayuanwang@gmail.com>
2020-02-27 13:24:56 -0500

    bug fix

--- compute_ev.py
+++ compute_ev.py
@@ -22,13 +22,12 @@ def compute_ev(stim, subj, roi=""):
     data = np.load("output/cortical_voxel_across_sessions_subj%02d%s.npy" % (subj, roi))
     ev_list = []
     for v in tqdm(range(data.shape[1])):
-        repeat = np.array([data[np.array(l[i]),v] for i in range(3)]).T
+        repeat = np.array([data[np.array(l[i]), v] for i in range(3)]).T
         try:
             assert repeat.shape == (1000,3)
         except AssertionError:
             print(repeat.shape)
         ev_list.append(ev(repeat))
-
     return ev_list
 
 
@@ -46,6 +45,6 @@ if __name__ == "__main__":
     stim = pd.read_pickle("/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl")
 
     all_evs = compute_ev(stim, args.subj, roi)
-    np.save("output/evs_subj%02d%s.npy" % (args.subj, roi))
+    np.save("output/evs_subj%02d%s.npy" % (args.subj, roi), all_evs)
     plt.hist(all_evs)
     plt.savefig("figures/evs_subj%02d%s.npy")
