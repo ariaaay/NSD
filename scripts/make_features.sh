e2066bdbb52bdba082b69469beb5ada2fefaffbb
ariaaay <ariayuanwang@gmail.com>
2020-03-02 12:51:30 -0500

    udpate task list

--- scripts/make_features.sh
+++ scripts/make_features.sh
@@ -1,24 +1,27 @@
-TASKS="autoencoder \
- denoise \
- segment25d \
- segment2d \
- curvature \
- class_1000 \
+TASKS="class_1000 \
  class_places \
  edge2d \
- edge3d \
- keypoint2d \
- keypoint3d \
- reshade \
- rgb2depth \
- rgb2mist \
- rgb2sfnorm \
- colorization \
- room_layout \
- segmentsemantic \
- vanishing_point \
- jigsaw \
- inpainting_whole"
+ edge3d"
+
+#TASKS="segment25d \
+# segment2d \
+# reshade \
+# rgb2mist \
+# rgb2sfnorm \
+# colorization \
+# jigsaw"
+
+#TASKS="curvature \
+#autoencoder \
+#denoise \
+#inpainting_whole
+#keypoint2d \
+#keypoint3d \
+#rgb2depth \
+#room_layout \
+#segmentsemantic \
+#vanishing_point \
+#"
 
 for task in $TASKS; do
  sbatch ~/NSD/scripts/generate_taskonomy_features.sh $task
