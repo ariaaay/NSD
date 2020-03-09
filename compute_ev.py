d328a4b160b80067bce6223770960df90378fe79
ariaaay <ariayuanwang@gmail.com>
2020-03-08 20:03:33 -0400

    compute ev over 10000

--- compute_ev.py
+++ compute_ev.py
@@ -1,4 +1,5 @@
 import argparse
+import pickle
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -7,7 +8,7 @@ import matplotlib.pyplot as plt
 from util.util import ev
 
 # extract each subject's index for 1000 images
-def extract_subject_trials_index_common(stim, subj):
+def extract_subject_trials_index_shared1000(stim, subj):
     index = list()
     for i in range(3):
         col = 'subject%01d_rep%01d' % (subj, i)
@@ -17,17 +18,25 @@ def extract_subject_trials_index_common(stim, subj):
     return index
 
 
+def extract_subject_trials_index(stim, subj):
+    # load trial index
+    trial_index_path = "~/NSD/output/trials_subj%02d.pkl" % args.subj
+    trial_lists = pickle.load(open(trial_index_path))
+    return trial_lists,
+
 def compute_ev(stim, subj, roi="", biascorr=False, zscored_input=False):
-    l = extract_subject_trials_index_common(stim, subj)
+    l = extract_subject_trials_index(stim, subj)
+    repeat_n = len(l[-1])
     if zscored_input:
         data = np.load("output/cortical_voxel_across_sessions_zscored_by_run_subj%02d%s.npy" % (subj, roi))
     else:
         data = np.load("output/cortical_voxel_across_sessions_subj%02d%s.npy" % (subj, roi))
+
     ev_list = []
-    for v in tqdm(range(data.shape[1])):
+    for v in tqdm(range(data.shape[1])): #loop over voxels
         repeat = np.array([data[np.array(l[i]), v] for i in range(3)]).T
         try:
-            assert repeat.shape == (1000,3)
+            assert repeat.shape == (repeat_n,3)
         except AssertionError:
             print(repeat.shape)
         ev_list.append(ev(repeat, biascorr=biascorr))
@@ -42,30 +51,28 @@ if __name__ == "__main__":
     parser.add_argument("--zscored_input", action="store_true")
 
     args = parser.parse_args()
+
+    tag = ""
+
     if args.roi_only:
         roi = "_roi_only"
-    else:
-        roi = ""
+        tag += roi
 
     if args.biascorr:
-        bs = "_biascorr"
-    else:
-        bs = ""
+        tag += "_biascorr"
 
     if args.zscored_input:
-        zs = "_zscored"
-    else:
-        zs = ""
+        tag += "_zscored"
 
     stim = pd.read_pickle("/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl")
     try:
-        all_evs = np.load("output/evs_subj%02d%s%s%s.npy" % (args.subj, roi, bs, zs))
+        all_evs = np.load("output/evs_subj%02d%s.npy" % (args.subj, tag))
     except FileNotFoundError:
         print("computing EVs")
         all_evs = compute_ev(stim, args.subj, roi, args.biascorr, args.zscored_input)
-        np.save("output/evs_subj%02d%s%s%s.npy" % (args.subj, roi, bs, zs), all_evs)
+        np.save("output/evs_subj%02d%s.npy" % (args.subj, tag), all_evs)
 
     plt.figure()
     plt.hist(all_evs)
-    plt.title("Explainable Variance across Voxels (subj%02d%s%s%s)" % (args.subj, roi, bs, zs))
-    plt.savefig("figures/evs_subj%02d%s%s%s.png" % (args.subj, roi, bs, zs))
+    plt.title("Explainable Variance across Voxels (subj%02d%s)" % (args.subj, tag))
+    plt.savefig("figures/evs_subj%02d%s.png" % (args.subj, tag))
