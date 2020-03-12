40e8faaca2df9f3058e57bd67c1284447ca59b80
ariaaay <ariayuanwang@gmail.com>
2020-03-11 23:25:00 -0400

    add task running scripts

---
+++ scripts/run_encoding_models.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#SBATCH --job-name=enco
+#SBATCH -p gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=2
+#SBATCH --mem=10G
+#SBATCH --time=10-00:00:00
+#SBATCH --error=/home/yuanw3/error_log/job.%J.err
+#SBATCH --output=/home/yuanw3/error_log/job.%J.out
+
+set -eu
+source venv/bin/activate
+module load cuda-10.1
+module load cudnn-10.1-v7.6.5.32
+
+task=$1
+
+cd code
+sub=1
+
+echo "running taskonomy $task task on subject $sub"
+python run_modeling.py --model taskrepr_$task --whole_brain --subj $sub --fix_testing --notest
---
+++ scripts/run_models.sh
@@ -0,0 +1,28 @@
+TASKS="class_1000 \
+ class_places \
+ edge2d \
+ edge3d"
+
+#TASKS="rgb2sfnorm \
+#segment25d \
+#segment2d \
+#reshade \
+#curvature \
+#autoencoder \
+#denoise \
+#inpainting_whole
+#keypoint2d \
+#keypoint3d \
+#rgb2depth \
+#room_layout \
+#segmentsemantic \
+#vanishing_point \
+#"
+
+#TASKS="rgb2mist \
+# colorization \
+# jigsaw"
+
+for task in $TASKS; do
+ sbatch ~/NSD/scripts/run_encoding_models.sh $task
+done
