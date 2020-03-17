c9c85003388bb169a03212a39817d1773c976f98
ariaaay <ariayuanwang@gmail.com>
2020-03-16 23:32:14 -0400

    generate more featmat for tayer and tayer scripts

---
+++ scripts/run_encoding_models_on_tayer.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+source venv/bin/activate
+
+
+TASKS="class_1000 \
+class_places \
+edge2d \
+edge3d \
+rgb2sfnorm \
+segment25d \
+segment2d \
+reshade \
+curvature \
+autoencoder \
+denoise \
+inpainting_whole
+keypoint2d \
+keypoint3d \
+rgb2depth \
+room_layout \
+segmentsemantic \
+vanishing_point \
+"
+
+sub=1
+
+for task in $TASKS; do
+  echo "running taskonomy $task task on subject $sub"
+  python nsd_code/run_modeling.py --model taskrepr_$task --subj $sub --fix_testing --notest
+done
--- scripts/run_models.sh
+++ scripts/run_models.sh
@@ -1,27 +1,26 @@
-TASKS="class_1000 \
- class_places \
- edge2d \
- edge3d"
+#TASKS="class_1000 \
+# class_places \
+# edge2d \
+# edge3d"
 
-#TASKS="rgb2sfnorm \
-#segment25d \
-#segment2d \
-#reshade \
-#curvature \
-#autoencoder \
-#denoise \
-#inpainting_whole
-#keypoint2d \
-#keypoint3d \
-#rgb2depth \
-#room_layout \
-#segmentsemantic \
-#vanishing_point \
-#"
+TASKS="rgb2sfnorm \
+segment25d \
+segment2d \
+reshade \
+curvature \
+autoencoder \
+denoise \
+inpainting_whole
+keypoint2d \
+keypoint3d \
+rgb2depth \
+room_layout \
+segmentsemantic \
+vanishing_point \
+rgb2mist \
+colorization"
 
-#TASKS="rgb2mist \
-# colorization \
-# jigsaw"
+# jigsaw
 
 for task in $TASKS; do
  sbatch ~/NSD/scripts/run_encoding_models.sh $task
