f60559fed0ee31c232cda83aa19cb4e2e42ed88d
ariaaay <ariayuanwang@gmail.com>
2020-03-25 10:52:13 -0400

    permutation on convnet scripts

---
+++ scripts/run_permutation_tests_on_convnet.sh
@@ -0,0 +1,9 @@
+source venv/bin/activate
+
+ MODELS="vgg16
+ resnet50"
+
+for model in $MODELS; do
+  echo "running convnet $model on subject 1"
+  python nsd_code/run_modeling.py --model convnet_$model --subj 1  --test --permute_y
+done
