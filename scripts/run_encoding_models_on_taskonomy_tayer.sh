06b6f530f5300a373e2f9dc980bf687de54faf25
ariaaay <ariayuanwang@gmail.com>
2020-04-29 22:23:40 -0400

    update to have subject variable

--- scripts/run_encoding_models_on_taskonomy_tayer.sh
+++ scripts/run_encoding_models_on_taskonomy_tayer.sh
@@ -3,33 +3,30 @@
 source venv/bin/activate
 
 
-#TASKS="class_1000 \
-#class_places \
-#edge2d \
-#edge3d \
-#rgb2sfnorm \
-#segment25d \
-#segment2d \
-#reshade \
-#curvature \
-#autoencoder \
-#denoise \
-#inpainting_whole
-#keypoint2d \
-#keypoint3d \
-#rgb2depth \
-#room_layout \
-#segmentsemantic \
-#vanishing_point \
-#"
-
-TASKS="inpainting_whole \
+TASKS="class_1000 \
+vanishing_point \
+room_layout \
+class_places \
+edge2d \
+edge3d \
+rgb2sfnorm \
+segment25d \
+segment2d \
+reshade \
+curvature \
+autoencoder \
+denoise \
+inpainting_whole
+keypoint2d \
+keypoint3d \
+rgb2depth \
+segmentsemantic \
 colorization \
 rgb2mist"
 
 #jigsaw
 
-sub=1
+sub=$1
 
 for task in $TASKS; do
   echo "running taskonomy $task task on subject $sub"
