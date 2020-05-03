63ae144d43581b9e193fcf2d0f95a105784e3733
ariaaay <ariayuanwang@gmail.com>
2020-05-03 19:35:50 -0400

    update scripts

--- scripts/run_taskonomy_models.sh
+++ scripts/run_taskonomy_models.sh
@@ -1,23 +1,23 @@
-#TASKS="class_1000 \
-# class_places \
-# edge2d \
-# edge3d \
-# rgb2sfnorm \
-# segment25d \
-# segment2d \
-# reshade \
-# curvature \
-# autoencoder \
-# denoise \
-# inpainting_whole
-# keypoint2d \
-# keypoint3d \
-# rgb2depth \
-# room_layout \
-# segmentsemantic \
-# vanishing_point"
-# rgb2mist \
-# colorization"
+TASKS="class_1000 \
+ class_places \
+ edge2d \
+ edge3d \
+ rgb2sfnorm \
+ segment25d \
+ segment2d \
+ reshade \
+ curvature \
+ autoencoder \
+ denoise \
+ inpainting_whole
+ keypoint2d \
+ keypoint3d \
+ rgb2depth \
+ room_layout \
+ segmentsemantic \
+ vanishing_point \
+ rgb2mist \
+ colorization"
 
 #jigsaw"
 
