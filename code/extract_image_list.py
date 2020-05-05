78c13bbcd612c05faaaf75349a5337223da0161c
ariaaay <ariayuanwang@gmail.com>
2020-05-04 23:11:35 -0400

    delete deprecated function

--- code/extract_image_list.py
+++ code/extract_image_list.py
@@ -40,19 +40,10 @@ def extract_repeat_trials_list(stim, subj):
     return all_rep_trials_list
 
 
-# def extract_img_list(stim, subj):
-#     col_name = "subject%1d" % (subj)
-#     image_id_list = list(stim.cocoId[stim[col_name] != 0])
-#     return image_id_list
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--subj", type=int)
     parser.add_argument("--type", type=str)
-    parser.add_argument(
-        "--all_images", type=bool, help="Return all images a subject sees"
-    )
     # parser.add_argument("--rep", type=int, default="0", help="Choose which repeats (0-2)")
 
     args = parser.parse_args()
@@ -60,11 +51,6 @@ if __name__ == "__main__":
     stim = pd.read_pickle(
         "/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl"
     )
-
-    # if args.all_images:
-    #     image_list = extract_img_list(stim, args.subj)
-    #     with open("output/coco_ID_subj%02d.pkl" % (args.subj), "wb") as f:
-    #         pickle.dump(image_list, f)
 
     if args.type == "cocoId":
         image_list = extract_repeat_img_list(stim, args.subj)
