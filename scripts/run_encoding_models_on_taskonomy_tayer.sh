bb1248a5b9d2446c3254f47fddf62ea208633ba9
ariaaay <ariayuanwang@gmail.com>
2020-05-05 16:30:22 -0400

    add subject options

--- scripts/run_encoding_models_on_taskonomy_tayer.sh
+++ scripts/run_encoding_models_on_taskonomy_tayer.sh
@@ -7,32 +7,28 @@ TASKS="vanishing_point \
 room_layout \
 class_places \
 edge2d \
-edge3d"
-#class_1000 \
-#rgb2sfnorm \
-#segment25d \
-#segment2d \
-#reshade \
-#curvature \
-#autoencoder \
-#denoise \
-#inpainting_whole
-#keypoint2d \
-#keypoint3d \
-#rgb2depth \
-#segmentsemantic \
-#colorization \
-#rgb2mist"
+edge3d \
+class_1000 \
+rgb2sfnorm \
+segment25d \
+segment2d \
+reshade \
+curvature \
+autoencoder \
+denoise \
+inpainting_whole
+keypoint2d \
+keypoint3d \
+rgb2depth \
+segmentsemantic \
+colorization \
+rgb2mist"
 
 #jigsaw
 
-SUBJS="5 \
-7"
+subj=$1
 
-#sub=$1
-for sub in $SUBJS; do
-  for task in $TASKS; do
-    echo "running taskonomy $task task on subject $sub"
-    python code/run_modeling.py --model taskrepr_$task --subj $sub --fix_testing --notest
-  done
+for task in $TASKS; do
+  echo "running taskonomy $task task on subject $subj"
+  python code/run_modeling.py --model taskrepr_$task --subj $subj --fix_testing --notest
 done
