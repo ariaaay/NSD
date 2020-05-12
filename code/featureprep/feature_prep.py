e1c652f6f4d595781cbe9238d93d306c48fcb44c
ariaaay <ariayuanwang@gmail.com>
2020-05-11 20:43:19 -0400

    load all subj features from subj specific folder

--- code/featureprep/feature_prep.py
+++ code/featureprep/feature_prep.py
@@ -23,10 +23,7 @@ def get_features(subj, stim_list, model):
     # file name.
 
     try:
-        if subj == 1:
-            featmat = np.load("features/%s.npy" % model)
-        else:
-            featmat = np.load("features/subj%d/%s.npy" % (subj, model))
+        featmat = np.load("features/subj%d/%s.npy" % (subj, model))
     except FileNotFoundError:
         if "taskrepr" in model:
             # latent space in taskonomy, model should be in the format of "taskrepr_X", e.g. taskrep_curvature
