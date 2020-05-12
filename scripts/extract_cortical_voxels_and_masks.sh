7848492c20c002c16d8f18af8c853787c3fe0d91
ariaaay <ariayuanwang@gmail.com>
2020-05-11 23:23:32 -0400

    add scripts to extract cortical voxels

---
+++ scripts/extract_cortical_voxels_and_masks.sh
@@ -0,0 +1,12 @@
+SUBJS="5 \
+      7"
+for subj in $SUBJS; do
+  python code/extract_cortical_voxel.py --zscore_by_run --subj $subj
+
+  # extract ROI mask to apply on cortical data
+  python code/extract_cortical_voxel.py --subj $subj --mask_only --roi prf-eccrois
+  python code/extract_cortical_voxel.py --subj $subj --mask_only --roi prf-visualrois
+  python code/extract_cortical_voxel.py --subj $subj --mask_only --roi floc-faces
+  python code/extract_cortical_voxel.py --subj $subj --mask_only --roi floc-words
+  python code/extract_cortical_voxel.py --subj $subj --mask_only --roi floc-places
+done
--- scripts/project_command.sh
+++ scripts/project_command.sh
@@ -1,9 +1,24 @@
 subj=1
-
+# extract trial ID list
 python code/extract_image_list.py --subj $subj --type trial
+
+# prepare brain voxels for encoding models:
+#   - extract cortical mask;
+#   - mask volume metric data;
+#   - zscore data by runs
 python code/extract_cortical_voxel.py --zscore_by_run --subj $subj
 
+# extract ROI mask to apply on cortical data
+python code/extract_cortical_voxel.py --subj $subj --mask_only --roi prf-eccrois
+python code/extract_cortical_voxel.py --subj $subj --mask_only --roi prf-visualrois
+python code/extract_cortical_voxel.py --subj $subj --mask_only --roi floc-faces
+python code/extract_cortical_voxel.py --subj $subj --mask_only --roi floc-words
+python code/extract_cortical_voxel.py --subj $subj --mask_only --roi floc-places
+
+# computer explainable variance for the data and output data averaged by repeats
 python code/compute_ev.py --subj $subj --zscored_input
+
+# run encoding models on taskonomy (on tayer)
 . scripts/run_encoding_models_on_taskonomy_tayer.sh $subj
 
 python code/process_permuation_results.py --subj $subj
