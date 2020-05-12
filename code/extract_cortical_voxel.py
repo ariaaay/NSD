f08374d59236c7331d32566d545ab0fd285991be
ariaaay <ariayuanwang@gmail.com>
2020-05-11 22:47:34 -0400

    save non boolean ROI 1d mask

--- code/extract_cortical_voxel.py
+++ code/extract_cortical_voxel.py
@@ -59,11 +59,10 @@ def extract_cortical_mask(subj, roi=""):
 
         # save a 1D version as well
         cortical = nsd_cortical_mat > -1
-        roi_1d_mask = (anat_mat[cortical]).astype(bool)
+        roi_1d_mask = (anat_mat[cortical])
         # assert np.sum(roi_1d_mask) == np.sum(mask)
-        print(np.sum(roi_1d_mask))
-        print(np.sum(mask))
-        print(len(cortical))
+        print("Number of non-zero ROI voxels: " + str(np.sum(roi_1d_mask>0)))
+        print("Number of cortical voxels is: " + len(cortical))
         assert len(roi_1d_mask) == np.sum(
             cortical
         )  # check the roi 1D length is same as cortical numbers in nsd general
