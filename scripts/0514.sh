a2d68964148439bb257cd246cb8fce58b382b17e
ariaaay <ariayuanwang@gmail.com>
2020-05-14 01:43:39 -0400

    add quick scripts

--- scripts/0514.sh
+++ scripts/0514.sh
@@ -1,3 +1,5 @@
 python code/run_modeling.py --model taskrepr_edge3d --subj 1 --fix_testing --notest
+python code/run_modeling.py --model res_of_taskrepr_edge3d_from_taskrepr_inpainting_whole --subj 1 fix_testing --notest
+python code/run_modeling.py --model res_of_taskrepr_inpainting_whole_from_taskrepr_edge3d --subj 1 fix_testing --notest
 python code/run_modeling.py --model pred_of_taskrepr_edge3d_from_taskrepr_inpainting_whole --subj 1 fix_testing --notest
 python code/run_modeling.py --model pred_of_taskrepr_inpainting_whole_from_taskrepr_edge3d --subj 1 fix_testing --notest
