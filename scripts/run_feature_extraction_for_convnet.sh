b23eff4d5ddc2f65157199977289442a7ae83dd3
ariaaay <ariayuanwang@gmail.com>
2020-05-18 18:04:25 -0400

    modify feature extraction scripts

--- scripts/run_feature_extraction_for_convnet.sh
+++ scripts/run_feature_extraction_for_convnet.sh
@@ -1,9 +1,11 @@
-LAYERS="conv2 \
+LAYERS="conv1 \
+conv2 \
 conv3 \
 conv4 \
 conv5 \
-fc6"
+fc6 \
+fc7"
 
 for layer in $LAYERS; do
- sbatch ~/NSD/scripts/feature_extraction.sh $layer
+ sbatch ~/NSD/scripts/feature_extraction.sh $layer --model alexnet --subsampling_size 10000
 done
