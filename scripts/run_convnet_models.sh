8f722e6904b807106068a3fc3347c999fcc61070
ariaaay <ariayuanwang@gmail.com>
2020-05-19 19:04:45 -0400

    update scripts to generate features for convnet layers

--- scripts/run_convnet_models.sh
+++ scripts/run_convnet_models.sh
@@ -1,5 +1,10 @@
-FEATURES="vgg16 \
-res50
+FEATURES="alexnet_conv1 \
+alexnet_conv2 \
+alexnet_conv3 \
+alexnet_conv4 \
+alexnet_conv5 \
+alexnet_fc6 \
+alexnet_fc7
 "
 
 for feature in $FEATURES; do
