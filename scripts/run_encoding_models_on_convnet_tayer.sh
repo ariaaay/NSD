e92130ecc31002f0c327e64b9054b7d0031c0883
ariaaay <ariayuanwang@gmail.com>
2020-07-26 17:04:16 -0400

    run on tayer

--- scripts/run_encoding_models_on_convnet_tayer.sh
+++ scripts/run_encoding_models_on_convnet_tayer.sh
@@ -2,14 +2,15 @@
 
 source venv/bin/activate
 
-FEATURES="alexnet_conv1_avgpool \
-alexnet_conv2_avgpool \
-alexnet_conv3_avgpool \
-alexnet_conv4_avgpool \
-alexnet_conv5_avgpool \
-alexnet_fc6_avgpool \
-alexnet_fc7__avgpool
-"
+#FEATURES="alexnet_conv1_avgpool \
+#alexnet_conv2_avgpool \
+#alexnet_conv3_avgpool \
+#alexnet_conv4_avgpool \
+#alexnet_conv5_avgpool \
+#alexnet_fc6_avgpool \
+#alexnet_fc7__avgpool
+#"
+FEATURES="res50"
 
 
 sub=1
