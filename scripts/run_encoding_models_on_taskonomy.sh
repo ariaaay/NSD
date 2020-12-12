7a9fb497e1893060385c4107f090a6e6be8f45c7
ariaaay <ariayuanwang@gmail.com>
2020-12-11 16:58:25 -0800

    update scripts for mind cluster

--- scripts/run_encoding_models_on_taskonomy.sh
+++ scripts/run_encoding_models_on_taskonomy.sh
@@ -13,8 +13,11 @@ source venv/bin/activate
 module load cuda-11.1.1
 module load cudnn-11.1.1-v8.0.4.30
 
-subj=$1
-task=$2
+# subj=$1
+# task=$2
 
-echo "running taskonomy $task task on subject $subj"
-python code/run_modeling.py --model taskrepr_$task --subj $subj --fix_testing --output_dir /user_data/yuanw3/project_outputs/NSD/output
+# echo "running taskonomy $task task on subject $subj"
+# python code/run_modeling.py --model taskrepr_$task --subj $subj --fix_testing --output_dir /user_data/yuanw3/project_outputs/NSD/output
+
+python code/run_modeling.py --model taskrepr_edge2d taskrepr_class_places --subj 1 --fix_testing --output_dir /user_data/yuanw3/project_outputs/NSD/output
+python code/run_modeling.py --model taskrepr_edge3d taskrepr_class_places --subj 1 --fix_testing --output_dir /user_data/yuanw3/project_outputs/NSD/output
