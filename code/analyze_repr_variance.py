6063e8205b0662b144cab0ac7a2573770681eaf3
ariaaay <ariayuanwang@gmail.com>
2020-12-15 17:47:52 -0800

    debug

--- code/analyze_repr_variance.py
+++ code/analyze_repr_variance.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import pandas as pd
 
 from featureprep.feature_prep import extact_feature_by_imgs
 
@@ -11,7 +12,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # coco ID - common 10000
-    stimulus_list = np.load("output/trials_subj01.npy")[:,0]
+    stim = pd.read_pickle("/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl")
+    stimulus_list = stim["cocoId"][stim["shared1000"]]
+    assert len(stimulus_list) == 1000
     feature_mat = extact_feature_by_imgs(stimulus_list, args.task)
     np.save("%s/%s_common_features.npy" % (args.output_path, args.task), feature_mat)
     
