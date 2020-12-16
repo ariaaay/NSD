847d62b470e3d792ab1e7dffaac02465fac48522
ariaaay <ariayuanwang@gmail.com>
2020-12-15 18:08:52 -0800

    debug

--- code/analyze_repr_variance.py
+++ code/analyze_repr_variance.py
@@ -5,24 +5,61 @@ import pandas as pd
 from featureprep.feature_prep import extact_feature_by_imgs
 
 
+def load_common_feature_matrix(task):
+    try:
+        feature_mat = np.load("%s/%s_common_features.npy" % (args.output_path, task))
+    except FileNotFoundError:
+        stim = pd.read_pickle(
+            "/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl"
+        )
+        stimulus_list = stim["cocoId"][stim["shared1000"]]
+        assert len(stimulus_list) == 1000
+        feature_mat = extact_feature_by_imgs(stimulus_list, task)
+        np.save("%s/%s_common_features.npy" % (args.output_path, task), feature_mat)
+    return feature_mat
+
+
+def measure_variance(feature_matrix):
+    cov = np.cov(feature_matrix)
+    eigv = np.linalg.eigvals(cov)
+    return np.sum(eigv)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--task")
-    parser.add_argument("--output_path", type=str, default="/user_data/yuanw3/project_outputs/NSD/output")
+    parser.add_argument("--task", default=None)
+    parser.add_argument(
+        "--task_list",
+        default=[
+            "edge2d",
+            "edge3d",
+            "keypoint2d",
+            "keypoint3d",
+            "class_places",
+            "class_1000",
+            "autoencoder",
+            "vaningshing_point",
+            "colorization",
+            "rgb2sfnorm",
+        ],
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        default="/user_data/yuanw3/project_outputs/NSD/output",
+    )
     args = parser.parse_args()
 
     # coco ID - common 1000
-    try:
-        feature_mat = np.load("%s/%s_common_features.npy" % (args.output_path, args.task))
-    except FileNotFoundError:
-        stim = pd.read_pickle("/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl")
-        stimulus_list = stim["cocoId"][stim["shared1000"]]
-        assert len(stimulus_list) == 1000
-        feature_mat = extact_feature_by_imgs(stimulus_list, args.task)
-        np.save("%s/%s_common_features.npy" % (args.output_path, args.task), feature_mat)
-    
-    cov = np.cov(feature_mat)
-    eigv = np.linalg.eigvals(cov)
-    print(np.sum(eigv))
 
-
+    if args.task_list is not None:
+        tasks = ["taskrepr_" + t for t in args.task_list]
+        var_dict = dict()
+        for t in tasks:
+            fm = load_common_feature_matrix(t)
+            var_dict[t] = measure_variance(fm)
+        print(var_dict)
+        
+    if args.task is not None:
+        fm = load_common_feature_matrix(args.task)
+        print(measure_variance(fm))
