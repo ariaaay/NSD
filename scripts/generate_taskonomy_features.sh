a23996b927401d70fe81f72cba626f7eb162f968
ariaaay <ariayuanwang@gmail.com>
2021-02-02 00:03:36 -0800

    add counter

--- scripts/generate_taskonomy_features.sh
+++ scripts/generate_taskonomy_features.sh
@@ -25,14 +25,16 @@ if ! [ -e $target_DIR ]; then
 	mkdir $target_DIR
 fi
 
+i=0
 while read p; do
-	echo "$p"
+	echo "$p $i/10000"
 	file_name=$p
 	store_name="$p.jpg"
 	img_file="$STIMULI_DIR/$store_name"
 	if [ ! -e $target_DIR/${file_name}_input_layer0.npy ]; then
 		python /home/yuanw3/taskonomy/taskbank/tools/run_img_task.py --task $task --img $imgfile --store "$target_DIR/$store_name" --store-all-rep
 	fi
+	((i=i+1))
 done </user_data/yuanw3/project_outputs/NSD/output/coco_ID_of_repeats_subj01.txt
 
 
