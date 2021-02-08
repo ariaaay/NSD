59ff716a63945d5c7f876cdfa9696254bf0a42fb
ariaaay <ariayuanwang@gmail.com>
2021-02-08 00:48:13 -0800

    update param

--- scripts/run_encoding_models_on_taskonomy_layers.sh
+++ scripts/run_encoding_models_on_taskonomy_layers.sh
@@ -17,5 +17,5 @@ module load cudnn-11.1.1-v8.0.4.30
 
 # echo "running taskonomy $task task on subject $subj"
 
-python code/run_modeling.py --model taskrepr_edge2d --layer 1 --subj 1 --fix_testing --output_dir /user_data/yuanw3/project_outputs/NSD/output
-python code/run_modeling.py --model taskrepr_edge2d --layer 4 --subj 1 --fix_testing --output_dir /user_data/yuanw3/project_outputs/NSD/output
+python code/run_modeling.py --model taskrepr_edge3d --layer 4 --subj 1 --fix_testing --output_dir /user_data/yuanw3/project_outputs/NSD/output
+python code/run_modeling.py --model taskrepr_edge3d --layer 5 --subj 1 --fix_testing --output_dir /user_data/yuanw3/project_outputs/NSD/output
