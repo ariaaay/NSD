82d6ad7312aab303136d73599b19c36f60d402cd
ariaaay <ariayuanwang@gmail.com>
2021-02-09 15:50:54 -0800

    add zscore

--- code/analyze_category_specificity.py
+++ code/analyze_category_specificity.py
@@ -132,26 +132,25 @@ if __name__ == "__main__":
     image_cat = np.load("../data/NSD_cat_feat.npy")
     image_supercat = np.load("../data/NSD_supcat_feat.npy")
 
-    similarity_features = ["text_embedding_of_labels"]
 
-    if "text_embedding_of_labels" in similarity_features:
-        import gensim.downloader as api
-        import ssl
+    # # text_embedding_of_labels
+    # import gensim.downloader as api
+    # import ssl
 
-        ssl._create_default_https_context = ssl._create_unverified_context
-        glove_twitter = api.load("glove-twitter-200")
+    # ssl._create_default_https_context = ssl._create_unverified_context
+    # glove_twitter = api.load("glove-twitter-200")
 
-        # here the rsm is either 80 by 80 or 12 by 12
-        emb_cat_sim, _ = make_text_sim_matrix(COCO_cat)
-        emb_supercat_sim, super_vocab = make_text_sim_matrix(COCO_super_cat)
+    # # here the rsm is either 80 by 80 or 12 by 12
+    # emb_cat_sim, _ = make_text_sim_matrix(COCO_cat)
+    # emb_supercat_sim, super_vocab = make_text_sim_matrix(COCO_super_cat)
 
-        plt.imshow(emb_cat_sim, cmap="YlOrRd")
-        plt.colorbar()
-        plt.savefig("../Cats/figures/rsm_COCOcat_text_embedding.png")
+    # plt.imshow(emb_cat_sim, cmap="YlOrRd")
+    # plt.colorbar()
+    # plt.savefig("../Cats/figures/rsm_COCOcat_text_embedding.png")
 
-        plt.imshow(emb_supercat_sim, cmap="YlOrRd")
-        plt.colorbar()
-        plt.savefig("../Cats/figures/rsm_COCOsupercat_text_embedding.png")
+    # plt.imshow(emb_supercat_sim, cmap="YlOrRd")
+    # plt.colorbar()
+    # plt.savefig("../Cats/figures/rsm_COCOsupercat_text_embedding.png")
 
     # Sample images
 
@@ -164,68 +163,77 @@ if __name__ == "__main__":
     img_ind = [list(nsd2coco).index(i) for i in cocoId_subj1]
     assert len(img_ind) == 10000
 
+
     # sort the images arrays with the order of maximum super cat
     image_supercat_subsample = image_supercat[img_ind, :]
     max_cat = np.argmax(image_supercat_subsample, axis=1)
     max_cat_order = np.argsort(max_cat)
     # plt.hist(max_cat)
 
-    if "object_areas" in similarity_features:
-        sorted_image_supercat = image_supercat_subsample[max_cat_order, :]
-        sorted_image_supercat_sim_by_image = cosine_similarity(sorted_image_supercat)
 
-        image_cat_subsample = image_cat[img_ind, :]
-        sorted_image_cat = image_cat_subsample[np.argsort(max_cat), :]
-        sorted_image_cat_sim_by_image = cosine_similarity(sorted_image_cat)
+    # object_areas
+    sorted_image_supercat = image_supercat_subsample[max_cat_order, :]
+    sorted_image_supercat_sim_by_image = cosine_similarity(sorted_image_supercat)
 
-        sorted_image_supercat_sim_by_categories = cosine_similarity(
-            sorted_image_supercat.T
+    image_cat_subsample = image_cat[img_ind, :]
+    sorted_image_cat = image_cat_subsample[np.argsort(max_cat), :]
+    sorted_image_cat_sim_by_image = cosine_similarity(sorted_image_cat)
+
+    sorted_image_supercat_sim_by_categories = cosine_similarity(
+        sorted_image_supercat.T
+    )
+    # normalize across categories?
+
+    plt.figure(figsize=(40, 20))
+    plt.subplot(1, 2, 1)
+    plt.imshow(sorted_image_supercat_sim_by_image, cmap="YlOrRd")
+    plt.colorbar()
+    plt.title("COCO super categories")
+    plt.subplot(1, 2, 2)
+    plt.imshow(sorted_image_cat_sim_by_image, cmap="YlOrRd")
+    plt.title("COCO basic categories")
+    plt.colorbar()
+    plt.savefig("../Cats/figures/rsm_COCOsupercat_object_areas.png")
+
+
+    # individua_ROIs
+    PPA = np.load("../output/rdms/subj01_places_PPA.npy")
+    OPA = np.load("../output/rdms/subj01_places_OPA.npy")
+    RSC = np.load("../output/rdms/subj01_places_RSC.npy")
+    FFA1 = np.load("../output/rdms/subj01_faces_FFA-1.npy")
+    FFA2 = np.load("../output/rdms/subj01_faces_FFA-1.npy")
+
+    brains = [PPA, OPA, RSC, FFA1, FFA2]
+
+    plt.figure(figsize=(10, 50))
+    for i, b in enumerate(brains):
+        plt.subplot(1, 5, i + 1)
+        plt.imshow(
+            b[:, max_cat_order][max_cat_order, :],
+            cmap="RdBu_r",
+            vmin=-0.5,
+            vmax=0.5,
         )
-        # normalize across categories?
 
-        plt.figure(figsize=(40, 20))
-        plt.subplot(1, 2, 1)
-        plt.imshow(sorted_image_supercat_sim_by_image, cmap="YlOrRd")
-        plt.colorbar()
-        plt.title("COCO super categories")
-        plt.subplot(1, 2, 2)
-        plt.imshow(sorted_image_cat_sim_by_image, cmap="YlOrRd")
-        plt.title("COCO basic categories")
-        plt.colorbar()
-        plt.savefig("../Cats/figures/rsm_COCOsupercat_object_areas.png")
+    plt.savefig("../Cats/figures/rsm_COCOsupercat_individual_ROIs.png")
 
-    if "individua_ROIs" in similarity_features:
-        PPA = np.load("../output/rdms/subj01_places_PPA.npy")
-        OPA = np.load("../output/rdms/subj01_places_OPA.npy")
-        RSC = np.load("../output/rdms/subj01_places_RSC.npy")
-        FFA1 = np.load("../output/rdms/subj01_faces_FFA-1.npy")
-        FFA2 = np.load("../output/rdms/subj01_faces_FFA-1.npy")
 
-        brains = [PPA, OPA, RSC, FFA1, FFA2]
+    # bert_caption
+    bert = np.load("../features/NSD_bert_all_layer_emb_subj1.npy")
+    bert = np.reshape(bert, (10000, 5 * 13 * 768))
+    
+    from util.util import zscore
+    bert = zscore(bert, axis=1)
 
-        plt.figure(figsize=(10, 50))
-        for i, b in enumerate(brains):
-            plt.subplot(1, 5, i + 1)
-            plt.imshow(
-                b[:, max_cat_order][max_cat_order, :],
-                cmap="RdBu_r",
-                vmin=-0.5,
-                vmax=0.5,
-            )
+    bert_sim = cosine_similarity(bert)
+    sorted_bert_sim = bert_sim[max_cat_order, :][:, max_cat_order]
 
-        plt.savefig("../Cats/figures/rsm_COCOsupercat_individual_ROIs.png")
 
-    if "bert_caption" in similarity_features:
-        bert = np.load("../features/NSD_bert_all_layer_emb_subj1.npy")
-        bert = np.reshape(bert, (10000, 5 * 13 * 768))
+    # all_rois
+    all_rois = np.load(
+        "../output/rdms/subj01_floc-words_floc-faces_floc-places_prf-visualrois.npy"
+    )
 
-        bert_sim = cosine_similarity(bert)
-        sorted_bert_sim = bert_sim[max_cat_order, :][:, max_cat_order]
-
-    if "all_rois" in similarity_features:
-        all_rois = np.load(
-            "../output/rdms/subj01_floc-words_floc-faces_floc-places_prf-visualrois.npy"
-        )
 
     # plot comparison
     plt.figure(figsize=(40, 20))
