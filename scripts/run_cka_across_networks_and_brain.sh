bafabb7584bbe521a5aa016ba3dea139035b9d12
ariaaay <ariayuanwang@gmail.com>
2021-03-04 14:55:27 -0800

    bug fix

--- scripts/run_cka_across_networks_and_brain.sh
+++ scripts/run_cka_across_networks_and_brain.sh
@@ -8,5 +8,5 @@ taskrepr_edge2d \
 taskrepr_edge3d
 "
 for network in $NETWORKS; do
- python python code/run_cka.py --cka_across_networks_and_brain $network
+ python code/run_cka.py --cka_across_networks_and_brain $network
 done
