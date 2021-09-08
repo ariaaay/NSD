7e5d15aaacee3e9f2a438544c5fc446bdf02e3ef
ariaaay <ariayuanwang@gmail.com>
2021-09-08 15:34:50 -0400

    add code to extract intermediate features

--- code/extract_clip_features.py
+++ code/extract_clip_features.py
@@ -1,5 +1,3 @@
-import clip
-import torch
 import json
 
 import pandas as pd
@@ -8,7 +6,13 @@ import numpy as np
 from tqdm import tqdm
 from PIL import Image
 
+import torch
+import torchvision
+
 from pycocotools.coco import COCO
+import torchextractor as tx
+
+import CLIP.clip as clip
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -32,34 +36,34 @@ def load_captions(cid):
 
 
 # Load Images
-stimuli_dir = "/lab_data/tarrlab/common/datasets/NSD_images"
+stimuli_dir = "/lab_data/tarrlab/common/datasets/NSD_images/images"
 
 stim = pd.read_pickle(
     "/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl"
 )
 all_coco_ids = stim.cocoId
-all_images_paths = list()
-all_images_paths += ["%s/%s.jpg" % (stimuli_dir, id) for id in all_coco_ids]
-print("Number of Images: {}".format(len(all_images_paths)))
 
 all_features = []
 model, preprocess = clip.load("ViT-B/32", device=device)
 
 # extract image features
-# for p in tqdm(all_images_paths):
-#     image = preprocess(Image.open(p)).unsqueeze(0).to(device)
-#         # text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
+all_images_paths = list()
+all_images_paths += ["%s/%s.jpg" % (stimuli_dir, id) for id in all_coco_ids]
+print("Number of Images: {}".format(len(all_images_paths)))
+for p in tqdm(all_images_paths):
+    image = preprocess(Image.open(p)).unsqueeze(0).to(device)
+        # text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
 
-#     with torch.no_grad():
-#         image_features = model.encode_image(image)
+    with torch.no_grad():
+        image_features = model.encode_image(image)
         
-#         # logits_per_image, logits_per_text = model(image, text)
-#         # probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+        # logits_per_image, logits_per_text = model(image, text)
+        # probs = logits_per_image.softmax(dim=-1).cpu().numpy()
 
-#     all_features.append(image_features.cpu().data.numpy())
-# all_features = np.array(all_features)
-# print(all_features.shape)
-# np.save("/lab_data/tarrlab/common/datasets/features/NSD/clip.npy", all_features)
+    all_features.append(image_features.cpu().data.numpy())
+all_features = np.array(all_features)
+print(all_features.shape)
+np.save("/lab_data/tarrlab/common/datasets/features/NSD/clip.npy", all_features)
 
 # extract text feature of image titles
 all_text_features = []
@@ -74,3 +78,21 @@ for cid in tqdm(all_coco_ids):
 all_text_features = np.array(all_text_features)
 print(all_text_features.shape)
 np.save("/lab_data/tarrlab/common/datasets/features/NSD/clip_text.npy", all_text_features)
+
+
+# intermediate features
+LOI_vision = ["visual.transformer.resblocks.%01d.ln_2" % i for i in range(11)]
+LOI_text  = ["transformer.resblocks.%01d.ln_2" % i for i in range(11)]
+
+model = tx.Extractor(model, LOI_vision + LOI_text)
+for cid in tqdm(all_coco_ids[0]):
+    with torch.no_grad():
+        image_path = "%s/%s.jpg" % (stimuli_dir, cid)
+        image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
+
+        captions = load_captions(cid)
+        text = clip.tokenize(captions).to(device)
+
+        logits, features = model(image, text)
+        feature_shapes = {name: f.shape for name, f in features.items()}
+        print(feature_shapes)
