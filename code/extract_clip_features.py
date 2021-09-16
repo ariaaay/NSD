76fb0dd449ba657f19380af42e528a3d7ab26ed4
ariaaay <ariayuanwang@gmail.com>
2021-09-16 16:40:30 -0400

    bug fix

--- code/extract_clip_features.py
+++ code/extract_clip_features.py
@@ -113,7 +113,7 @@ LOI_text  = ["transformer.resblocks.%01d.ln_2" % i for i in range(12)]
 # for text features
 text_features = [copy.copy(e) for _ in range(12) for e in [[]]]
 model = tx.Extractor(model, LOI_text)
-for cid in tqdm(all_coco_ids[:10]):
+for cid in tqdm(all_coco_ids[:5]):
     with torch.no_grad():
         image_path = "%s/%s.jpg" % (stimuli_dir, cid)
         image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
@@ -128,7 +128,7 @@ for cid in tqdm(all_coco_ids[:10]):
             for i, layer in enumerate(LOI_text):
                 layer_features[i].append(features[layer].cpu().data.numpy().squeeze().flatten())
 
-        print(np.array(layer_features).shape)
+            print(np.array(layer_features).shape)
         avg_features = np.mean(np.array(layer_features), axis=1) # 12 x m
 
     for i in range(len(LOI_text)):  
