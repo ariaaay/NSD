b0a770fef9d5adaac6b77c4621a003aef956d6fb
ariaaay <ariayuanwang@gmail.com>
2021-09-30 18:22:58 -0400

    script bug fix

--- scripts/run_encoding_models_on_clip_layers.sh
+++ scripts/run_encoding_models_on_clip_layers.sh
@@ -2,5 +2,5 @@ modality=$1
 for layer in {0..6}; do
   echo "running "${modality}_layer_${layer}" on subject 1"
   # python code/run_modeling.py --model "${modality}_layer_${layer}" --subj 1 --fix_testing
-  python code/run_modeling.py --model "visual_layer_resnet${layer}" --subj 1 --fix_testing
+  python code/run_modeling.py --model "visual_layer_resnet_${layer}" --subj 1 --fix_testing
 done
