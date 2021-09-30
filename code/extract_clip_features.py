f1e2a2e6b91f453dc9bf3c687b15561d60e18b27
ariaaay <ariayuanwang@gmail.com>
2021-09-30 18:02:13 -0400

    refactor code

--- code/extract_clip_features.py
+++ code/extract_clip_features.py
@@ -1,4 +1,5 @@
 import copy
+import argparse
 
 import pandas as pd
 import numpy as np
@@ -12,19 +13,12 @@ import torch
 import torchvision
 import torch.nn as nn
 
-from pycocotools.coco import COCO
 import torchextractor as tx
 
 import clip
 from util.util import pytorch_pca
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-
-trainFile = "/lab_data/tarrlab/common/datasets/coco_annotations/captions_train2017.json"
-valFile = "/lab_data/tarrlab/common/datasets/coco_annotations/captions_val2017.json"
-train_caps = COCO(trainFile)
-val_caps = COCO(valFile)
-
 
 def load_captions(cid):
     annIds = train_caps.getAnnIds(imgIds=[cid])
@@ -40,7 +34,21 @@ def load_captions(cid):
     return captions
 
 
-def extract_visual_resnet_feature():
+def extract_visual_resnet_prePCA_feature():
+    LOI_ResNet_vision = [
+        "visual.bn1",
+        "visual.avgpool",
+        "visual.layer1.2.bn3",
+        "visual.layer2.3.bn3",
+        "visual.layer3.5.bn3",
+        "visual.layer4.2.bn3",
+        "visual.attnpool",
+    ]
+    model, preprocess = clip.load("RN50", device=device)
+    model_visual = tx.Extractor(model, LOI_ResNet_vision)
+    compressed_features = [copy.copy(e) for _ in range(8) for e in [[]]]
+    subsampling_size = 5000
+
     print("Extracting ResNet features")
     for cid in tqdm(all_coco_ids):
         with torch.no_grad():
@@ -60,143 +68,231 @@ def extract_visual_resnet_feature():
                     # print(tmp.size())
                     compressed_features[i].append(tmp.squeeze().cpu().numpy().flatten())
                 else:
-                    compressed_features[i].append(f.squeeze().data.cpu().numpy().flatten())
+                    compressed_features[i].append(
+                        f.squeeze().data.cpu().numpy().flatten()
+                    )
 
-    compressed_features_array = []
     for l, f in enumerate(compressed_features):
         np.save("%s/visual_layer_resnet_prePCA_%01d.npy" % (feature_output_dir, l), f)
         # compressed_features_array.append(np.array(f))
 
-# Load Images
-subj = 1
+def extract_visual_resnet_prePCA_feature():
+    LOI_ResNet_vision = [
+        "visual.bn1",
+        "visual.avgpool",
+        "visual.layer1.2.bn3",
+        "visual.layer2.3.bn3",
+        "visual.layer3.5.bn3",
+        "visual.layer4.2.bn3",
+        "visual.attnpool",
+    ]
+    model, preprocess = clip.load("RN50", device=device)
+    model_visual = tx.Extractor(model, LOI_ResNet_vision)
+    compressed_features = [copy.copy(e) for _ in range(8) for e in [[]]]
+    subsampling_size = 5000
+
+    print("Extracting ResNet features")
+    for cid in tqdm(all_coco_ids):
+        with torch.no_grad():
+            image_path = "%s/%s.jpg" % (stimuli_dir, cid)
+            image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
+            captions = load_captions(cid)
+            text = clip.tokenize(captions).to(device)
+
+            _, features = model_visual(image, text)
+
+            for i, f in enumerate(features.values()):
+                # print(f.size())
+                if len(f.size()) > 3:
+                    c = f.data.shape[1]  # number of channels
+                    k = int(np.floor(np.sqrt(subsampling_size / c)))
+                    tmp = nn.functional.adaptive_avg_pool2d(f.data, (k, k))
+                    # print(tmp.size())
+                    compressed_features[i].append(tmp.squeeze().cpu().numpy().flatten())
+                else:
+                    compressed_features[i].append(
+                        f.squeeze().data.cpu().numpy().flatten()
+                    )
+
+    for l, f in enumerate(compressed_features):
+        np.save("%s/visual_layer_prePCA_%01d.npy" % (feature_output_dir, l), f)
+
+
+def extract_visual_resnet_feature():
+    for l in range(7):
+        try:
+            f = np.load(
+                "%s/visual_layer_resnet_prePCA_%01d.npy" % (feature_output_dir, l)
+            )
+        except FileNotFoundError:
+            extract_visual_resnet_prePCA_feature()
+            f = np.load(
+                "%s/visual_layer_resnet_prePCA_%01d.npy" % (feature_output_dir, l)
+            )
+
+        print("Running PCA")
+        print("feature shape: ")
+        print(f.shape)
+        pca = PCA(n_components=min(f.shape[0], 64), svd_solver="auto")
+
+        fp = pca.fit_transform(f)
+        print("Feature %01d has shape of:" % l)
+        print(fp.shape)
+
+        np.save("%s/visual_layer_resnet_%01d.npy" % (feature_output_dir, l), fp)
+
+
+def extract_visual_transformer_feature():
+    model, preprocess = clip.load("ViT-B/32", device=device)
+    LOI_transformer_vision = [
+        "visual.transformer.resblocks.%01d.ln_2" % i for i in range(12)
+    ]
+    model_visual = tx.Extractor(model, LOI_transformer_vision)
+    compressed_features = [copy.copy(e) for _ in range(12) for e in [[]]]
+
+    for cid in tqdm(all_coco_ids):
+        with torch.no_grad():
+            image_path = "%s/%s.jpg" % (stimuli_dir, cid)
+            image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
+            captions = load_captions(cid)
+            text = clip.tokenize(captions).to(device)
+
+            _, features = model_visual(image, text)
+
+            for i, f in enumerate(features.values()):
+                compressed_features[i].append(f.squeeze().cpu().data.numpy().flatten())
+
+    compressed_features = np.array(compressed_features)
+
+    for l, f in enumerate(compressed_features):
+        pca = PCA(n_components=min(f.shape[0], 64), whiten=True, svd_solver="full")
+        try:
+            fp = pca.fit_transform(f)
+        except ValueError:
+            print(fp.shape)
+
+        print("Feature %01d has shape of:" % l)
+        print(fp.shape)
+
+        np.save("%s/visual_layer_%01d.npy" % (feature_output_dir, l), fp)
+
+
+
+def extract_text_layer_feature():
+    from pycocotools.coco import COCO
+
+    trainFile = "/lab_data/tarrlab/common/datasets/coco_annotations/captions_train2017.json"
+    valFile = "/lab_data/tarrlab/common/datasets/coco_annotations/captions_val2017.json"
+    train_caps = COCO(trainFile)
+    val_caps = COCO(valFile)
+
+    model, preprocess = clip.load("ViT-B/32", device=device)
+    LOI_text = ["transformer.resblocks.%01d.ln_2" % i for i in range(12)]
+    text_features = [copy.copy(e) for _ in range(12) for e in [[]]]
+    model_text = tx.Extractor(model, LOI_text)
+    for cid in tqdm(all_coco_ids):
+        with torch.no_grad():
+            image_path = "%s/%s.jpg" % (stimuli_dir, cid)
+            image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
+
+            captions = load_captions(cid)
+
+            layer_features = [
+                copy.copy(e) for _ in range(12) for e in [[]]
+            ]  # layer_features is 12 x 5 x m
+            for caption in captions:
+                text = clip.tokenize(caption).to(device)
+                _, features = model_text(image, text)
+                # features is a feature dictionary for all layers, each image, each caption
+                for i, layer in enumerate(LOI_text):
+                    layer_features[i].append(
+                        features[layer].cpu().data.numpy().squeeze().flatten()
+                    )
+
+                # print(np.array(layer_features).shape)
+            avg_features = np.mean(np.array(layer_features), axis=1)  # 12 x m
+
+        for i in range(len(LOI_text)):
+            text_features[i].append(avg_features[i])
+
+    text_features = np.array(text_features)
+    # print(text_features.shape) # 12 x 10000 x m
+
+    for l, f in enumerate(text_features):
+        pca = PCA(n_components=min(f.shape[0], 64), whiten=True, svd_solver="full")
+        try:
+            fp = pca.fit_transform(f)
+        except ValueError:
+            print(fp.shape)
+
+        print("Feature %01d has shape of:" % l)
+        print(fp.shape)
+
+        np.save("%s/text_layer_%01d.npy" % (feature_output_dir, l), fp)
+
+
+def extract_last_layer_feature(model_name="ViT-B/32"):
+    all_features = []
+    model, preprocess = clip.load(model_name, device=device)
+
+    # extract image features
+    all_images_paths = list()
+    all_images_paths += ["%s/%s.jpg" % (stimuli_dir, id) for id in all_coco_ids]
+    print("Number of Images: {}".format(len(all_images_paths)))
+    for p in tqdm(all_images_paths):
+        image = preprocess(Image.open(p)).unsqueeze(0).to(device)
+        # text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
+
+        with torch.no_grad():
+            image_features = model.encode_image(image)
+
+        all_features.append(image_features.cpu().data.numpy())
+    all_features = np.array(all_features)
+    print(all_features.shape)
+    np.save(
+        "/lab_data/tarrlab/common/datasets/features/NSD/clip_visual_resnet.npy",
+        all_features,
+    )
+
+    # extract text feature of image titles
+    all_text_features = []
+    for cid in tqdm(all_coco_ids):
+        with torch.no_grad():
+            captions = load_captions(cid)
+            # print(captions)
+            text = clip.tokenize(captions).to(device)
+            cap_emb = model.encode_text(text).cpu().data.numpy()
+            all_text_features.append(cap_emb)
+
+    all_text_features = np.array(all_text_features)
+    print(all_text_features.shape)
+    np.save(
+        "/lab_data/tarrlab/common/datasets/features/NSD/clip_text.npy",
+        all_text_features,
+    )
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--subj", default=1, type=int)
+parser.add_argument(
+    "--feature_dir",
+    type=str,
+    default="/user_data/yuanw3/project_outputs/NSD/features",
+)
+parser.add_argument(
+    "--project_output_dir",
+    type=str,
+    default="/user_data/yuanw3/project_outputs/NSD/output",
+)
+args = parser.parse_args()
+
 
 stimuli_dir = "/lab_data/tarrlab/common/datasets/NSD_images/images"
-feature_output_dir = "/user_data/yuanw3/project_outputs/NSD/features/subj%01d" % subj
-project_output_dir = "/user_data/yuanw3/project_outputs/NSD/output"
-
-# stim = pd.read_pickle(
-#     "/lab_data/tarrlab/common/datasets/NSD/nsddata/experiments/nsd/nsd_stim_info_merged.pkl"
-# )
-# all_coco_ids = stim.cocoId
+feature_output_dir = "%s/subj%01d" % (args.feature_dir, args.subj)
 
 all_coco_ids = np.load(
-    "%s/coco_ID_of_repeats_subj%02d.npy" % (project_output_dir, subj)
+    "%s/coco_ID_of_repeats_subj%02d.npy" % (args.project_output_dir, args.subj)
 )
 
-all_features = []
-# model, preprocess = clip.load("ViT-B/32", device=device)
-model, preprocess = clip.load("RN50", device=device)
 
-# # extract image features
-# all_images_paths = list()
-# all_images_paths += ["%s/%s.jpg" % (stimuli_dir, id) for id in all_coco_ids]
-# print("Number of Images: {}".format(len(all_images_paths)))
-# for p in tqdm(all_images_paths):
-#     image = preprocess(Image.open(p)).unsqueeze(0).to(device)
-#         # text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
-
-#     with torch.no_grad():
-#         image_features = model.encode_image(image)
-
-#     all_features.append(image_features.cpu().data.numpy())
-# all_features = np.array(all_features)
-# print(all_features.shape)
-# np.save("/lab_data/tarrlab/common/datasets/features/NSD/clip_visual_resnet.npy", all_features)
-
-# extract text feature of image titles
-# all_text_features = []
-# for cid in tqdm(all_coco_ids):
-#     with torch.no_grad():
-#         captions = load_captions(cid)
-#         # print(captions)
-#         text = clip.tokenize(captions).to(device)
-#         cap_emb = model.encode_text(text).cpu().data.numpy()
-#         all_text_features.append(cap_emb)
-
-# all_text_features = np.array(all_text_features)
-# print(all_text_features.shape)
-# np.save("/lab_data/tarrlab/common/datasets/features/NSD/clip_text.npy", all_text_features)
-
-
-# intermediate features
-LOI_vision = ["visual.transformer.resblocks.%01d.ln_2" % i for i in range(12)]
-LOI_ResNet_vision = [
-    "visual.bn1",
-    "visual.avgpool",
-    "visual.layer1.2.bn3",
-    "visual.layer2.3.bn3",
-    "visual.layer3.5.bn3",
-    "visual.layer4.2.bn3",
-    "visual.attnpool",
-]
-LOI_text = ["transformer.resblocks.%01d.ln_2" % i for i in range(12)]
-
-# For visual features
-model_visual = tx.Extractor(model, LOI_ResNet_vision)
-compressed_features = [copy.copy(e) for _ in range(8) for e in [[]]]
-subsampling_size=5000
-
-
-
-for l in range(8):
-    try:
-        f = np.load("%s/visual_layer_resnet_prePCA_%01d.npy" % (feature_output_dir, l))
-    except FileNotFoundError:
-        extract_visual_resnet_feature()
-        f = np.load("%s/visual_layer_resnet_prePCA_%01d.npy" % (feature_output_dir, l))
-    
-    print("Running PCA")
-    print("feature shape: ")
-    print(f.shape)
-    pca = PCA(n_components=min(f.shape[0], 64), svd_solver="auto")
-
-    fp = pca.fit_transform(f)
-    print("Feature %01d has shape of:" % l)
-    print(fp.shape)
-
-    np.save("%s/visual_layer_resnet_%01d.npy" % (feature_output_dir, l), fp)
-
-    
-
-# # for text features
-# text_features = [copy.copy(e) for _ in range(12) for e in [[]]]
-# model_text = tx.Extractor(model, LOI_text)
-# for cid in tqdm(all_coco_ids):
-#     with torch.no_grad():
-#         image_path = "%s/%s.jpg" % (stimuli_dir, cid)
-#         image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
-
-#         captions = load_captions(cid)
-
-#         layer_features = [
-#             copy.copy(e) for _ in range(12) for e in [[]]
-#         ]  # layer_features is 12 x 5 x m
-#         for caption in captions:
-#             text = clip.tokenize(caption).to(device)
-#             _, features = model_text(image, text)
-#             # features is a feature dictionary for all layers, each image, each caption
-#             for i, layer in enumerate(LOI_text):
-#                 layer_features[i].append(
-#                     features[layer].cpu().data.numpy().squeeze().flatten()
-#                 )
-
-#             # print(np.array(layer_features).shape)
-#         avg_features = np.mean(np.array(layer_features), axis=1)  # 12 x m
-
-#     for i in range(len(LOI_text)):
-#         text_features[i].append(avg_features[i])
-
-# text_features = np.array(text_features)
-# # print(text_features.shape) # 12 x 10000 x m
-
-# for l, f in enumerate(text_features):
-#     pca = PCA(n_components=min(f.shape[0], 64), whiten=True, svd_solver="full")
-#     try:
-#         fp = pca.fit_transform(f)
-#     except ValueError:
-#         print(fp.shape)
-
-#     print("Feature %01d has shape of:" % l)
-#     print(fp.shape)
-
-#     np.save("%s/text_layer_%01d.npy" % (feature_output_dir, l), fp)
+extract_visual_resnet_feature()
