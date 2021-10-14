4e046de5dc8e7dca81f536a0dc005c8419eec8f2
ariaaay <ariayuanwang@gmail.com>
2021-10-14 13:00:45 -0400

    scripts update

--- scripts/run_encoding_models.sh
+++ scripts/run_encoding_models.sh
@@ -1,9 +1,13 @@
 # modality=$1
-for layer in {1..13}; do
-  echo "running "bert_layer_${layer}" on subject 1"
-  # python code/run_modeling.py --model "${modality}_layer_${layer}" --subj 1 --fix_testing
-  python code/run_modeling.py --model "bert_layer_${layer}" --subj 1 --fix_testing
-done
+# for layer in {1..13}; do
+#  echo "running "bert_layer_${layer}" on subject 1"
+#  # python code/run_modeling.py --model "${modality}_layer_${layer}" --subj 1 --fix_testing
+#  python code/run_modeling.py --model "bert_layer_${layer}" --subj 1 --fix_testing
+# done
 
-# python code/run_modeling.py --model "clip_object_caption_overlap" --subj 1 --fix_testing
-# python code/run_modeling.py --model "clip_top1_object" --subj 1 --fix_testing
+python code/run_modeling.py --model "cat" --subj 1 --fix_testing
+python code/run_modeling.py --model "supcat" --subj 1 --fix_testing
+python code/run_modeling.py --model "clip_text" --subj 1 --fix_testing
+python code/run_modeling.py --model "clip_visual_resnet" --subj 1 --fix_testing
+python code/run_modeling.py --model "clip_object" --subj 1 --fix_testing
+python code/run_modeling.py --model "clip_top1_object" --subj 1 --fix_testing
