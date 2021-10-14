4c2f187c41ccca24c4c512b0ba8689e294665f05
ariaaay <ariayuanwang@gmail.com>
2021-10-14 18:13:52 -0400

    scripts clean up

--- scripts/run_permutation_tests_on_convnet.sh
+++
@@ -1,8 +0,0 @@
-source venv/bin/activate
-
- MODELS="res50"
-
-for model in $MODELS; do
-  echo "running convnet $model on subject 1"
-  python code/run_modeling.py --model convnet_$model --subj 1  --test --permute_y
-done
