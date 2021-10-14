37cbd0ef0fd6704d66605356b38425ee917c1db9
ariaaay <ariayuanwang@gmail.com>
2021-10-14 18:21:08 -0400

    refactor scripts

---
+++ scripts/run_encoding_models_on_tayer.sh
@@ -0,0 +1,3 @@
+python code/run_modeling.py --model clip cat --subj 1 --fix_testing --output_dir ./output --features_dir ./features
+python code/run_modeling.py --model clip convnet_res50 --subj 1 --fix_testing --output_dir ./output --features_dir ./features
+python code/run_modeling.py --model convnet_res50 --subj 1 --fix_testing --output_dir ./output --features_dir ./features
