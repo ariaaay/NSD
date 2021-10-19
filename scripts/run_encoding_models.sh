39151c57c74978ab68995aa223744f76ffc0dcd4
ariaaay <ariayuanwang@gmail.com>
2021-10-19 12:43:45 -0400

    minor update

--- scripts/run_encoding_models.sh
+++ scripts/run_encoding_models.sh
@@ -8,11 +8,10 @@ subj=2
 
 python code/extract_clip_features.py --subj $subj
 
-python code/run_modeling.py --model "clip_text" --subj $subj --fix_testing
-python code/run_modeling.py --model "clip_object" --subj $subj --fix_testing
-python code/run_modeling.py --model "clip_top1_object" --subj $subj --fix_testing
+# python code/run_modeling.py --model "clip_object" --subj $subj --fix_testing
+# python code/run_modeling.py --model "clip_top1_object" --subj $subj --fix_testing
 
-python code/run_modeling.py --model "convnet_res50" --subj $subj --fix_testing
+# python code/run_modeling.py --model "convnet_res50" --subj $subj --fix_testing
 
 for layer in {0..11}; do
  echo "running "visual_layer_${layer}" on subject ${subj}"
@@ -20,8 +19,10 @@ for layer in {0..11}; do
 done
 
 for layer in {0..7}; do
- echo "running "visual_layer_${layer}" on subject ${subj}"
- python code/run_modeling.py --model "visual_layer_${layer}" --subj $subj --fix_testing
+ echo "running "visual_layer_resnet${layer}" on subject ${subj}"
+ python code/run_modeling.py --model "visual_layer_resnet${layer}" --subj $subj --fix_testing
 done
+python code/run_modeling.py --model "clip_text" --subj $subj --fix_testing
+
 
 # python code/run_modeling.py --model "bert_layer_13" --subj $subj --fix_testing
