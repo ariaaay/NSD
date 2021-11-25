987db2d6c41afebede22860e277eecf4c37c5179
ariaaay <ariayuanwang@gmail.com>
2021-11-25 00:19:21 -0500

    add newer shell scripts

--- scripts/run_encoding_models.sh
+++ scripts/run_encoding_models.sh
@@ -1,25 +1,9 @@
-
-
-
-# subset=$1
-# python code/run_modeling.py --model "clip" --subj $subj --fix_testing --subset_data ${subset}
-# python code/run_modeling.py --model "cat" --subj $subj --fix_testing --subset_data ${subset}
-# python code/run_modeling.py --model "convnet_res50" "clip" --subj $subj --fix_testing --subset_data ${subset}
-
-# python code/run_modeling.py --model "supcat" --subj $subj --fix_testing
-
 # python code/extract_clip_features.py --subj $subj
 
 # python code/run_modeling.py --model "clip_object" --subj $subj --fix_testing
 # python code/run_modeling.py --model "clip_top1_object" --subj $subj --fix_testing --subset_data "person"
 # python code/run_modeling.py --model "clip" --subj 1 --fix_testing 
-
-
-
-# python code/run_modeling.py --model "resnet50_bottleneck" --subj 2 --fix_testing
-# python code/run_modeling.py --model "resnet50_bottleneck" --subj 2 --fix_testing --subset_data "person"
-
-
+# python code/run_modeling.py --model "supcat" --subj $subj --fix_testing
 
 # for layer in {0..11}; do
 #  echo "running "visual_layer_${layer}" on subject ${subj}"
@@ -31,18 +15,48 @@
 #  python code/run_modeling.py --model "visual_layer_resnet${layer}" --subj $subj --fix_testing
 # done
 # python code/run_modeling.py --model "clip_text" --subj $subj --fix_testing
+MODELS="
+clip \
+clip_visual_resnet \
+clip_text \
+bert_layer_13 \
+resnet50_bottlenecks"
 
+for subj in {1,2,5,7}; do
+    for model in $MODELS; do
+        FILE=/user_data/yuanw3/project_outputs/NSD/output/encoding_results/subj$subj/corr_${model}_whole_brain_.p
+        if test -f "$FILE"; then
+            echo "$FILE exists."
+        else
+            python code/run_modeling.py --model $model --subj $subj --fix_testing
+        fi
+    done
 
-# subj=5
+    FILE=/user_data/yuanw3/project_outputs/NSD/output/encoding_results/subj$subj/corr_clip_visual_resnet_resnet50_bottleneck_whole_brain_.p
+    if test -f "$FILE"; then
+        echo "$FILE exists."
+    else
+        FILE=/user_data/yuanw3/project_outputs/NSD/output/encoding_results/subj$subj/corr_resnet50_bottleneck_clip_visual_resnet_whole_brain_.p
+        if test -f "$FILE"; then
+            echo "$FILE exists."
+        else
+            python code/run_modeling.py --model "clip_visual_resnet" "resnet50_bottleneck" --subj $subj --fix_testing
+        fi
+    fi
 
-# python code/run_modeling.py --model "clip" --subj $subj --fix_testing
-# python code/run_modeling.py --model "bert_layer_13" "clip" --subj $subj --fix_testing
-# python code/run_modeling.py --model "bert_layer_13" --subj $subj --fix_testing
-# python code/run_modeling.py --model "resnet50_bottleneck" --subj $subj --fix_testing
-# python code/run_modeling.py --model "resnet50_bottleneck" "clip" --subj $subj --fix_testing
+    FILE=/user_data/yuanw3/project_outputs/NSD/output/encoding_results/subj$subj/corr_clip_text_bert_layer_13_whole_brain_.p
+    if test -f "$FILE"; then
+        echo "$FILE exists."
+    else
+        FILE=/user_data/yuanw3/project_outputs/NSD/output/encoding_results/subj$subj/corr_bert_layer_13_clip_text_whole_brain_.p
+        if test -f "$FILE"; then
+            echo "$FILE exists."
+        else
+            python code/run_modeling.py --model "clip_text" "bert_layer_13" --subj $subj --fix_testing
+        fi
+    fi
 
-subj=5
-
+done
 # python code/run_modeling.py --model "clip" --subj $subj --fix_testing
 # python code/run_modeling.py --model "bert_layer_13" "clip" --subj $subj --fix_testing
 python code/run_modeling.py --model "bert_layer_13" --subj $subj --fix_testing
@@ -50,6 +64,5 @@ python code/run_modeling.py --model "resnet50_bottleneck" --subj $subj --fix_tes
 # python code/run_modeling.py --model "resnet50_bottleneck" "clip" --subj $subj --fix_testing
 
 
-# python code/run_modeling.py --model "clip_visual_resnet" --subj 2 --fix_testing
 
 
