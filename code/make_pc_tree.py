523034a1ee1c9486ca794af28e3226d77728521b
ariaaay <ariayuanwang@gmail.com>
2022-06-08 17:23:31 -0400

    add split ratio and consistency check

--- code/make_pc_tree.py
+++ code/make_pc_tree.py
@@ -3,10 +3,11 @@ import argparse
 import numpy as np
 
 import cortex
+from torch import true_divide
 from visualize_in_pycortex import project_vals_to_3d
 
 
-def split(original, pca_voxel_idxes, i_PC, cortical_n, branch="", split_threshold=5):
+def split(subj, original, pca_voxel_idxes, i_PC, cortical_n, branch="", split_threshold=5, split_ratio=9):
     """
     Input:
         original: PC projection matrix to be split by the ith PC (20000 x 20)
@@ -17,33 +18,72 @@ def split(original, pca_voxel_idxes, i_PC, cortical_n, branch="", split_threshol
         idx_A: coritical length mask to pick for matrix A
         vol_A: the volume correspond to matrix_A
     """
+    if len(pca_voxel_idxes) < 50:
+        return
     if i_PC < split_threshold:
-    # positive
         idx = np.where(original[:,i_PC]>0)[0] # n x 1 integer (n = # of chosen voxels)
-        if len(idx) == 0:
-            return
         matrix_A = original[idx] # n x 20
-        mask_A= np.zeros(cortical_n).astype(bool) # 100k x 1
         idx_A = pca_voxel_idxes[idx] # n x 1 integers, used to index cortical length array to pick out voxels relevant in this pc split
-        mask_A [idx_A] = True
-        branch += "A"
-        VOLS["PC %d %s" % (i_PC, branch)] = make_volume(args.subj, matrix_A[:,i_PC], mask_A)
-        split(matrix_A, idx_A, i_PC+1, cortical_n, branch)
-
-        # negative
         idx = np.where(original[:,i_PC]<0)[0]
-        if len(idx) == 0:
-            return
         matrix_B = original[idx]
-        mask_B = np.zeros(cortical_n).astype(bool)
         idx_B = pca_voxel_idxes[idx]
-        mask_B [idx_B] = True
-        branch += "B"
-        VOLS["PC %d %s" % (i_PC, branch)] = make_volume(args.subj, matrix_B[:,i_PC], mask_B)
-        split(matrix_B, idx_B, i_PC+1, cortical_n, branch)    
+        if not split_here(idx_A, idx_B, ratio=split_ratio): # this split is ineffective and should skip to the next PC
+            split(subj,original, pca_voxel_idxes, i_PC+1, cortical_n, branch + "X")
+        
+
+        # positive
+        mask_A= np.zeros(cortical_n).astype(bool) # 100k x 1
+        mask_A[idx_A] = True
+        VOLS["PC %d %s (n=%d)" % (i_PC, branch + "A", len(idx_A))] = make_volume(subj, matrix_A[:,i_PC], mask_A)
+        
+        # negative
+        mask_B = np.zeros(cortical_n).astype(bool)
+        mask_B[idx_B] = True
+        VOLS["PC %d %s (n=%d)" % (i_PC, branch + "B", len(idx_B))] = make_volume(subj, matrix_B[:,i_PC], mask_B)
+
+        split(subj, matrix_A, idx_A, i_PC+1, cortical_n, branch + "A")
+        split(subj, matrix_B, idx_B, i_PC+1, cortical_n, branch + "B")
 
 
-def make_volume(subj, vals, pca_mask, vmin=-2, vmax=2, cmap="BrBG_r", descale=True):
+def split_here(idx_A, idx_B, ratio):
+    if (len(idx_A) < 5) or (len(idx_B) < 5):
+        return False
+    elif len(idx_A) / len(idx_B) > ratio:
+        return False
+    elif len(idx_B) / len(idx_A) > ratio:
+        return False
+    else:
+        return True
+
+
+def make_subj_tree(subj, visualize=False):
+    subj_proj = np.load(
+        "%s/output/pca/%s/%s/subj%02d/pca_projections.npy"
+        % (OUTPUT_ROOT, args.model, args.name_modifier, subj)
+    ).T # 100k x 20 vectors
+    subj_mask = np.load(
+        "%s/output/pca/%s/%s/pca_voxels/pca_voxels_subj%02d.npy"
+        % (OUTPUT_ROOT, args.model, args.name_modifier, subj)
+    ) # 1 x 100k vectors
+
+    cortical_mask = np.load(
+        "%s/output/voxels_masks/subj%d/cortical_mask_subj%02d.npy"
+        % (OUTPUT_ROOT, subj, subj)
+    ) # 1 x 100k vectors
+
+    proj_val_only = subj_proj[subj_mask, :]
+    proj_val_only /= proj_val_only.std(axis=0)
+    pca_voxel_idxes = np.where(subj_mask==True)[0]
+
+    split(subj, subj_proj[subj_mask, :], pca_voxel_idxes, i_PC=0, cortical_n=np.sum(cortical_mask), split_threshold=5)
+    if visualize:
+        subj_port = "2111" + str(subj)
+        cortex.webgl.show(data=VOLS, port=int(subj_port), recache=False)
+        import pdb
+        pdb.set_trace()
+
+
+def make_volume(subj, vals, pca_mask, vmin=-0.1, vmax=0.1, cmap="BrBG_r"):
     import cortex
 
     mask = cortex.utils.get_cortical_mask(
@@ -54,8 +94,6 @@ def make_volume(subj, vals, pca_mask, vmin=-2, vmax=2, cmap="BrBG_r", descale=Tr
         % (OUTPUT_ROOT, subj, subj)
     )
     cortical_vals = np.zeros(np.sum(cortical_mask))*np.nan
-    # print(len(vals))
-    # print(np.sum(pca_mask))
     cortical_vals[pca_mask] = vals
     
 
@@ -80,39 +118,31 @@ if __name__ == "__main__":
     parser.add_argument("--subj", default=1, type=int)
     parser.add_argument("--model", default="clip")
     parser.add_argument("--name_modifier", default="best_20000_nc")
+    parser.add_argument("--on_cluster", default=False, action="store_true")
     parser.add_argument
 
     args = parser.parse_args()
     
     OUTPUT_ROOT = "."
-
-    # visualize PC projections
-    subj_proj = np.load(
-        "%s/output/pca/%s/%s/subj%02d/pca_projections.npy"
-        % (OUTPUT_ROOT, args.model, args.name_modifier, args.subj)
-    ).T # 100k x 20 vectors
-    subj_mask = np.load(
-        "%s/output/pca/%s/%s/pca_voxels/pca_voxels_subj%02d.npy"
-        % (OUTPUT_ROOT, args.model, args.name_modifier, args.subj)
-    ) # 1 x 100k vectors
-
-    cortical_mask = np.load(
-        "%s/output/voxels_masks/subj%d/cortical_mask_subj%02d.npy"
-        % (OUTPUT_ROOT, args.subj, args.subj)
-    ) # 1 x 100k vectors
-
-    proj_val_only = subj_proj[subj_mask, :]
-    proj_val_only /= proj_val_only.std(axis=0)
-    pca_voxel_idxes = np.where(subj_mask==True)[0]
-
+    if args.on_cluster:
+        OUTPUT_ROOT = "/user_data/yuanw3/project_outputs/NSD"
+    
     VOLS = {}
-    split(subj_proj[subj_mask, :], pca_voxel_idxes, i_PC=0, cortical_n=np.sum(cortical_mask), split_threshold=5)
-    subj_port = "7111" + str(args.subj)
-    # cortex.webgl.show(data=volumes, autoclose=False, port=int(subj_port))
-    cortex.webgl.show(data=VOLS, port=int(subj_port), recache=False)
+    # visualize single subject
+    # make_subj_tree(args.subj, visualize=True)
 
+    # compute consistency of trees
+    from analyze_in_mni import analyze_data_correlation_in_mni
+    subjs = np.arange(9)
+    all_volumes = []
+    for s in subjs:
+        make_subj_tree(s)
+        subj_volumes = VOLS.values()
+        labels = VOLS.keys()
+        all_volumes.append(subj_volumes)
+        VOLS = {}
+    # remember to run `module load fsl-6.0.3` on cluster
+    analyze_data_correlation_in_mni(all_volumes, args.model, save_name = "PC_tree_%s" % args.name_modifier, subjs=np.arange(1,9), volumes=True, xtick_label=labels)
 
-    import pdb
-
-    pdb.set_trace()
+    
 
