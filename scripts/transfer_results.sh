5ad7f4d403444edf2b8e871f17a64d65ba6a694c
ariaaay <ariayuanwang@gmail.com>
2023-03-14 15:16:12 -0400

    add scritps to transfer results

--- scripts/transfer_results.sh
+++ scripts/transfer_results.sh
@@ -1,21 +1,40 @@
 MIND_DIR="/user_data/yuanw3/project_outputs/NSD/output/encoding_results"
-LOCAl_DIR="~/Projects/NSD/output/encoding_results/"
+LOCAl_DIR="output/encoding_results"
 
-MODELS=(YFCC_clip \
+MODELS="YFCC_clip \
 YFCC_simclr \
-YFCC_slip)
+YFCC_slip"
 
 for subj in {1..8}; do
-    OUTDIR=$LOCAl_DIR/subj$subj/
+    echo $subj
+    OUTDIR=$LOCAl_DIR/subj$subj
     for model in $MODELS; do
         INFILE=$MIND_DIR/subj$subj/rsq_${model}_whole_brain.p
-        scp yuanw3@mind.cs.cmu.edu:$INFILE $OURDIR 
+        scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR 
     done
 
     INFILE=$MIND_DIR/subj$subj/rsq_YFCC_slip_YFCC_simclr_whole_brain.p
-    scp yuanw3@mind.cs.cmu.edu:$INFILE $OURDIR 
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR 
 
     INFILE=$MIND_DIR/subj$subj/rsq_YFCC_simclr_YFCC_clip_whole_brain.p
-    scp yuanw3@mind.cs.cmu.edu:$INFILE $OURDIR 
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR
+
+    INFILE=$MIND_DIR/subj$subj/rsq_YFCC_slip_clip_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+
+    INFILE=$MIND_DIR/subj$subj/rsq_laion2b_clip_laion400m_clip_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+
+    INFILE=$MIND_DIR/subj$subj/rsq_laion2b_clip_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+
+    INFILE=$MIND_DIR/subj$subj/rsq_laion400m_clip_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+
+    INFILE=$MIND_DIR/subj$subj/rsq_clip_laion400m_clip_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+
+    INFILE=$MIND_DIR/subj$subj/rsq_YFCC_clip_layer_n-1_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
 
 done
