0f1e49a82351b868d663c5509ccc8e73fc9b8525
ariaaay <ariayuanwang@gmail.com>
2023-04-19 14:57:14 -0400

    update results transfer scripts

--- scripts/transfer_results.sh
+++ scripts/transfer_results.sh
@@ -6,6 +6,11 @@ YFCC_simclr \
 YFCC_slip \
 IC_title_clip \
 IC_title_tag_description_clip
+clip \
+resnet50_bottleneck \
+laion2b_clip \
+laion400m_clip \
+clip_visual_resnet
 "
 
 for subj in {1..8}; do
@@ -16,29 +21,32 @@ for subj in {1..8}; do
         scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR 
     done
 
-    # INFILE=$MIND_DIR/subj$subj/rsq_YFCC_slip_YFCC_simclr_whole_brain.p
-    # scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR 
+    INFILE=$MIND_DIR/subj$subj/rsq_YFCC_slip_YFCC_simclr_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR 
 
-    # INFILE=$MIND_DIR/subj$subj/rsq_YFCC_simclr_YFCC_clip_whole_brain.p
-    # scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR
+    INFILE=$MIND_DIR/subj$subj/rsq_YFCC_simclr_YFCC_clip_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR
 
-    # INFILE=$MIND_DIR/subj$subj/rsq_YFCC_slip_clip_whole_brain.p
-    # scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+    INFILE=$MIND_DIR/subj$subj/rsq_YFCC_slip_clip_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
 
-    # INFILE=$MIND_DIR/subj$subj/rsq_laion2b_clip_laion400m_clip_whole_brain.p
-    # scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+    INFILE=$MIND_DIR/subj$subj/rsq_laion2b_clip_laion400m_clip_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
 
-    # INFILE=$MIND_DIR/subj$subj/rsq_laion2b_clip_whole_brain.p
-    # scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+    INFILE=$MIND_DIR/subj$subj/rsq_clip_laion400m_clip_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
 
-    # INFILE=$MIND_DIR/subj$subj/rsq_laion400m_clip_whole_brain.p
-    # scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+    INFILE=$MIND_DIR/subj$subj/rsq_YFCC_clip_layer_n-1_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR
 
-    # INFILE=$MIND_DIR/subj$subj/rsq_clip_laion400m_clip_whole_brain.p
-    # scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR  
+    INFILE=$MIND_DIR/subj$subj/rsq_clip_visual_resnet_resnet50_bottleneck_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR
 
-    # INFILE=$MIND_DIR/subj$subj/rsq_YFCC_clip_layer_n-1_whole_brain.p
-    # scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR
+    INFILE=$MIND_DIR/subj$subj/rsq_IC_title_clip_resnet50_bottleneck_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR
+
+    INFILE=$MIND_DIR/subj$subj/rsq_IC_title_tag_description_clip_resnet50_bottleneck_whole_brain.p
+    scp yuanw3@mind.cs.cmu.edu:$INFILE $OUTDIR
     
 
 done
